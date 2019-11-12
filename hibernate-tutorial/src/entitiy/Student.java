3e2368a673af6d5bd7a7b8c0ccf6f20206ccb14d
Asad Nawaz <asadnawazmobilecrazy@gmail.com>
2019-11-12 17:25:09 -0600

    created and tested connection, created basic student entity with JPA annotations

---
+++ hibernate-tutorial/src/entitiy/Student.java
@@ -0,0 +1,73 @@
+package entitiy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Student() {
+
+    }
+
+    public Student(String firstname, String lastName, String email) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ hibernate-tutorial/src/jdbc/TestJDBC.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        String user ="hbstudent";
+        String pass = "hbstudent";
+        try {
+            System.out.println("Connecting to database: "+ jdbcURL);
+
+            Connection conn = DriverManager.getConnection(jdbcURL, user, pass);
+
+            System.out.println("Connection Successful");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
